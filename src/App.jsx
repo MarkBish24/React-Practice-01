b590f285a409f4837c710b3566a63217d14c5415
MarkBish24 <markbish24@gmail.com>
2025-03-31 10:04:08 -0700

    Completed up to section 50

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcepts.jsx";
+import TabButton from "./components/TabButton.jsx";
+
 function App() {
   return (
     <div>
@@ -18,6 +20,15 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
         </section>
       </main>
     </div>
